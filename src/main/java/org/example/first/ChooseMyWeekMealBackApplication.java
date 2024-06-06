2d53bc1b5852517bb31ff5e6d7b3675144f5748d
Goby33 <martin.gobert@hotmail.fr>
2024-06-06 16:40:41 +0200

    first commit

---
+++ src/main/java/org/example/first/ChooseMyWeekMealBackApplication.java
@@ -0,0 +1,19 @@
+package org.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseMyWeekMealBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChooseMyWeekMealBackApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+    //run Hello world
+
+
+    // override run method
+
+}
